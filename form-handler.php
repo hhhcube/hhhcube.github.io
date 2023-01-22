827890b564b81dae4171af31769c2b4aea687261
Humphrey Hills <konnectedja@gmail.com>
2023-01-22 08:53:44 -0500

    Updated form-handler

--- form-handler.php
+++ form-handler.php
@@ -1,15 +1,34 @@
-<form action="form-handler.php" method="post">
-    <div class="form-group">
-        <label for="name">Name:</label>
-        <input type="text" class="form-control" id="name" name="name" required>
-    </div>
-    <div class="form-group">
-        <label for="email">Email:</label>
-        <input type="email" class="form-control" id="email" name="email" required>
-    </div>
-    <div class="form-group">
-        <label for="message">Message:</label>
-        <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<?php
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $errors = array();
+
+    if(empty($name)){
+        array_push($errors, "Name is required");
+    }else if(strlen($name) < 3){
+        array_push($errors, "Name must be at least 3 characters");
+    }
+
+    if(empty($email)){
+        array_push($errors, "Email is required");
+    }else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        array_push($errors, "Email must be a valid email address");
+    }
+
+    if(empty($message)){
+        array_push($errors, "Message is required");
+    }else if(strlen($message) < 10){
+        array_push($errors, "Message must be at least 10 characters");
+    }
+
+    if(count($errors) == 0){
+        // Send email
+    }else{
+        foreach ($errors as $error) {
+            echo $error . "<br>";
+        }
+    }
+}
